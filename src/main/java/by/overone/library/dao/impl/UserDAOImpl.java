ddea3b2291ce564d786b8faf6dc78364a7102eb4
fomabb <fomabb@yandex.by>
2022-02-14 20:14:47 +0300

    update methods

--- src/main/java/by/overone/library/dao/impl/UserDAOImpl.java
+++ src/main/java/by/overone/library/dao/impl/UserDAOImpl.java
@@ -16,6 +16,7 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
